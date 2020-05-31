702e859b5ac916290ef5cbe26a1711df9d89ed60
danielmarques28 <danielmarques7@hotmail.com>
2020-05-31 02:00:50 -0300

    Add auth in user routes (except in createUser)

--- api/app/routes/userRoutes.js
+++ api/app/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const verifyToken = require('../helpers/auth');
 
 router
-  .get('/', userController.indexUser)
+  .get('/', verifyToken, userController.indexUser)
   .post('/', userController.createUser)
-  .get('/:id', userController.getUser)
-  .put('/:id', userController.editUser)
-  .delete('/:id', userController.deleteUser);
+  .get('/:id', verifyToken, userController.getUser)
+  .put('/:id', verifyToken, userController.editUser)
+  .delete('/:id', verifyToken, userController.deleteUser);
 
 module.exports = router;
