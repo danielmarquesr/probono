2de9977ebe1e28f0a10b09c04794aa1692b68893
danielmarques28 <danielmarques7@hotmail.com>
2020-06-04 01:16:19 -0300

    Create Login and Register views

---
+++ front/src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <Background>
+    <div class="login">
+      <form>
+        oi: {{this.email}}
+        <Input
+          v-model="this.email"
+        />
+      </form>
+    </div>
+  </Background>
+</template>
+
+<script>
+import Background from '@/components/Background';
+import Input from '@/components/form/Input';
+
+export default {
+  name: 'Login',
+  components: {
+    Background,
+    Input
+  },
+  data: {
+    email: ''
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  padding: 0 20px;
+}
+</style>
---
+++ front/src/views/Register.vue
@@ -0,0 +1,24 @@
+<template>
+  <Background>
+    <div class="register">
+      Registrar-se
+    </div>
+  </Background>
+</template>
+
+<script>
+import Background from '@/components/Background';
+
+export default {
+  name: 'Register',
+  components: {
+    Background
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.register {
+  padding: 0 20px;
+}
+</style>
