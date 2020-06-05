d40bfe647fc1c5cad775d68f3f7d5652e2f2451c
danielmarques28 <danielmarques7@hotmail.com>
2020-06-05 16:34:37 -0300

    Treat CORS in server side

--- api/config/express.js
+++ api/config/express.js
@@ -7,8 +7,20 @@ dotenv.config();
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Authorization,Content-type,Accept,X-Custom-Header");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
 app.use(bodyParser.json());
 
-app.use('/api/v1.0', routes);
+app.use('/api/v1', routes);
 
 module.exports = app;
