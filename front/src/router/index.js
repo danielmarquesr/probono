75f943bc27477027ef49d72f93f32187f4562b60
danielmarques28 <danielmarques7@hotmail.com>
2020-06-05 17:06:07 -0300

    Create Lawsuit and Progress routes

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+
+// VIEWS
 import Home from '@/views/Home';
+import Lawsuit from '@/views/Lawsuit';
+import Client from '@/views/Client';
 import Login from '@/views/Login';
 import Register from '@/views/Register';
+import Progress from '@/views/Progress';
 
 Vue.use(VueRouter);
 
@@ -10,17 +15,38 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: 'processos',
+        name: 'Lawsuit',
+        component: Lawsuit,
+        // children: [
+        //   {
+        //     path: ':lawsuitId/andamentos',
+        //     name: 'Progress',
+        //     component: Progress
+        //   }
+        // ]
+      },
+      {
+        path: 'processos/:lawsuitId/andamentos',
+        name: 'Progress',
+        component: Progress
+      }
+    ]
   },
   {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
+    props: (route) => ({ query: route.query.q })
   },
   {
     path: '/registrar',
     name: 'Register',
-    component: Register
+    component: Register,
+    props: { type: 'client' }
   }
 ];
 
