7aab945277691a92ed2fcee7dc6da13919eae5eb
danielmarques28 <danielmarques7@hotmail.com>
2020-06-05 16:30:28 -0300

    Add loginClient and loginLawyer routes

--- api/app/routes/authRoutes.js
+++ api/app/routes/authRoutes.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/login', authController.login);
+router
+  .post('/login/client', authController.loginClient)
+  .post('/login/lawyer', authController.loginLawyer);
 
 module.exports = router;
