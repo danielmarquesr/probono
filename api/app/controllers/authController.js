b6d6e3bf5e59ce994dd676694013c66277ddc590
danielmarques28 <danielmarques7@hotmail.com>
2020-06-05 16:29:45 -0300

    Create login methods to Client and Lawyer

--- api/app/controllers/authController.js
+++ api/app/controllers/authController.js
@@ -6,13 +6,20 @@ const controller = {};
 
 const verifyPassword = (password, hash) => bcrypt.compareSync(password, hash);
 
-controller.login = async (req, res) => {
+const sendNotFount = () => {
+  res.status(404).json(
+    { status: 404, message: 'Wrong email or password !' },
+  );
+};
+
+controller.loginLawyer = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ where: { email } });
   const hash = user.dataValues.password;
   if (user && verifyPassword(password, hash)) {
     const { id } = user.dataValues;
-    const token = jwt.sign({ id }, process.env.SECRET, {
+    const role = 'laywer';
+    const token = jwt.sign({ id, role }, process.env.SECRET, {
       expiresIn: 18000,
     });
     res.status(200).json({ token });
@@ -21,6 +28,22 @@ controller.login = async (req, res) => {
       { status: 404, message: 'Wrong email or password !' },
     );
   }
+};
+
+controller.loginClient = async (req, res) => {
+  const { cpf, password } = req.body;
+  const userClient = await Client.findOne({ where: { cpf }, include: { model: User }});
+  if (userClient) {
+    const hash = userClient.dataValues.User.dataValues.password;
+    if(verifyPassword(password, hash)) {
+      const id = userClient.dataValues.userId;
+      const role = 'client';
+      const token = jwt.sign({ id, role }, process.env.SECRET, {
+        expiresIn: 18000,
+      });
+      res.status(200).json({ token });
+    } else sendNotFount();
+  } else sendNotFount();
 };
 
 module.exports = controller;
