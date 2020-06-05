c1c1b3f6316f9184ba5b11c109bb86cfc4482e2b
danielmarques28 <danielmarques7@hotmail.com>
2020-06-05 16:57:42 -0300

    Create the base of Home page

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/images/logo.png">
+    <div class="home-body">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Home'
-  }
+export default {
+  name: 'Home'
+}
 </script>
 
 <style lang="scss" scoped>
-
+.home {
+  width: 100vw;
+  height: 100vh;
+  background: #e5e9ea;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &-body {
+    width: 80vw;
+    height: 80vh;
+    padding: 0.7rem;
+    background: white;
+    border-radius: 5px;
+    box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+}
 </style>
