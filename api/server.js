2245bd3a2280ebf0bbb64f1fe56ce0220ad5bb5b
danielmarques28 <danielmarques7@hotmail.com>
2020-06-06 00:05:02 -0300

    Improve server log

--- api/server.js
+++ api/server.js
@@ -4,5 +4,7 @@ const port = process.env.PORT;
 
 app.listen(port, () => {
   /* eslint no-console: ["error", { allow: ["log"] }] */
-  console.log('\nExpress server running in port', port, '\n');
+  console.log('\n=======================================\n');
+  console.log('  Express server running in port', port, '\n');
+  console.log('=======================================\n');
 });
