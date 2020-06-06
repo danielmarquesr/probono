0524aedb0b3fcd249c3f2bacd4fdd1d5d680c069
danielmarques28 <danielmarques7@hotmail.com>
2020-06-06 00:08:57 -0300

    Create showProgressTranslation in progressController

--- api/app/controllers/progressController.js
+++ api/app/controllers/progressController.js
@@ -1,10 +1,26 @@
-const { Lawsuit, Progress } = require('../models');
+const { Translation, Progress } = require('../models');
 const controller = {};
 
 controller.indexProgressOfLawsuit = async (req, res) => {
   const lawsuitId = req.params.id;
   const progress = await Progress.findAll({ where: { lawsuitId } });
   res.status(200).json(progress);
-}
+};
+
+controller.showProgressTranslation = async (req, res) => {
+  const { id } = req.params;
+  const attributes = { attributes: { exclude: ['createdAt', 'updatedAt'] } };
+  const progressTranslation = await Progress.findOne(
+    {
+      where: { id },
+      attributes,
+      include: {
+        model: Translation,
+        attributes,
+      }
+    }
+  );
+  res.status(200).json(progressTranslation);
+};
 
 module.exports = controller;
--- api/app/routes/progressRoutes.js
+++ api/app/routes/progressRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const progressController = require('../controllers/progressController');
 
 router
-  .get('/lawsuit/:id', progressController.indexProgressOfLawsuit);
+  .get('/lawsuit/:id', progressController.indexProgressOfLawsuit)
+  .get('/:id/translation', progressController.showProgressTranslation);
 
 module.exports = router;
