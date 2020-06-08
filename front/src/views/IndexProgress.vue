4e4beeed873a36bfda5f2bfc4b9210227768512f
danielmarques28 <danielmarques7@hotmail.com>
2020-06-07 23:08:28 -0300

    Add button to open create modal

--- front/src/views/IndexProgress.vue
+++ front/src/views/IndexProgress.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="progress">
+    <ButtonField
+      :width="'200px'"
+      :click="() => {showModal = true}"
+    >
+      CRIAR
+    </ButtonField>
+
+    <ProgressModal
+      v-model="showModal"
+      @refreshProgress="indexProgress"
+    />
+
     <div
       class="progress-card"
       v-for="progress in progresses"
@@ -8,35 +20,45 @@
     >
       <b>id:</b> {{ progress.id }}
       <br />
-      <b>title:</b> {{ progress.title }}
-      <br />
       <b>date:</b> {{ progress.date }}
+      <br />
+      <b>description:</b> {{ progress.description }}
       <br /><br />
     </div>
   </div>
 </template>
 
 <script>
+import ProgressModal from '@/components/ProgressModal';
+import ButtonField from '@/components/form/ButtonField';
 import progressAPI from '@/api/progress';
 
 export default {
   name: 'IndexProgress',
+  components: {
+    ProgressModal,
+    ButtonField
+  },
   data() {
     return {
+      showModal: false,
       progresses: []
     }
   },
   created() {
-    const { lawsuitId } = this.$route.params;
-    progressAPI.indexProgressOfLawsuit(lawsuitId)
-      .then(res => {
-        this.progresses = res.data;
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    this.indexProgress();
   },
   methods: {
+    indexProgress() {
+      const { lawsuitId } = this.$route.params;
+      progressAPI.indexProgressOfLawsuit(lawsuitId)
+        .then(res => {
+          this.progresses = res.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
     redirectToProgressShow(progressId) {
       this.$router.push({ name: 'ShowProgress', params: { progressId } });
     }
--- front/src/views/IndexTranslation.vue
+++ front/src/views/IndexTranslation.vue
@@ -1,10 +1,70 @@
 <template>
-  <div></div>
+  <div class="index-translation">
+    <ButtonField
+      :width="'200px'"
+      :click="() => {showModal = true}"
+    >
+      CRIAR
+    </ButtonField>
+
+    <TranslationModal
+      v-model="showModal"
+      @refreshTranslation="getTranslationsOfLawyer"
+    />
+
+    <div
+      v-for="translation in translations"
+      :key="translation.id"
+    >
+      <div
+        class="translation-card"
+        v-on:click="() => redirectToShowTranslation(translation.id)"
+      >
+        id: {{ translation.id }}
+        <br />
+        explanation: {{ translation.explanation }}
+        <br />
+        textTarget: {{ translation.textTarget }}
+        <br /><br />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import translationAPI from '@/api/translation';
+import TranslationModal from '@/components/TranslationModal';
+import ButtonField from '@/components/form/ButtonField';
+
 export default {
-  name: 'IndexTranslation'
+  name: 'IndexTranslation',
+  components: {
+    TranslationModal,
+    ButtonField
+  },
+  data() {
+    return {
+      showModal: false,
+      translations: []
+    }
+  },
+  created() {
+    this.getTranslationsOfLawyer()
+  },
+  methods: {
+    getTranslationsOfLawyer() {
+      translationAPI.indexTranslation()
+        .then(res => {
+          this.translations = res.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    redirectToShowTranslation(translationId) {
+      this.$router.push({ name: 'ShowTranslation', params: { translationId } });
+    }
+  }
 }
 </script>
 
