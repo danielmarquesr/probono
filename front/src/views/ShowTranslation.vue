73f2da93a7bdeaffa077498a410601dba59d7162
danielmarques28 <danielmarques7@hotmail.com>
2020-06-07 23:05:38 -0300

    Create ShowTranslation view

---
+++ front/src/views/ShowTranslation.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="show-translation">
+    <div
+      v-for="progress in translation.Progresses"
+      :key="progress.id"
+    >
+      <div
+        class="progress-card"
+        v-on:click="() => redirectToShowProgress(progress.id, progress.lawsuitId)"
+      >
+        id: {{ progress.id }}
+        <br />
+        title: {{ progress.title }}
+        <br />
+        date: {{ progress.date }}
+        <br />
+        description: {{ progress.description }}
+        <br /><br />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import translationAPI from '@/api/translation';
+
+export default {
+  name: 'ShowTranslation',
+  data() {
+    return {
+      translation: []
+    }
+  },
+  created() {
+    const { translationId } = this.$route.params;
+    translationAPI.showTranslationProcess(translationId)
+      .then(res => {
+        this.translation = res.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  },
+  methods: {
+    redirectToShowProgress(progressId, lawsuitId) {
+      this.$router.replace({ name: 'ShowProgress', params: { progressId, lawsuitId } });
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
